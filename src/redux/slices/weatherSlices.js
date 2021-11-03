ed3597d0915b13831ff519034191d1873d8a9214
tohirbek009 <odilovtohirbek009@gmail.com>
2021-11-03 19:21:19 +0500

    weather app is ready

--- src/redux/slices/weatherSlices.js
+++ src/redux/slices/weatherSlices.js
@@ -8,7 +8,6 @@ export const fetchWeatherAction = createAsyncThunk(
     async (payload, {rejectWithValue, getState, dispatch})=>{
         try{
             const {data} = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${payload}&appid=${process.env.REACT_APP_OPEN_WEATHER_KEY}`);
-           
             return data
         }catch(error){
             if(!error?.response){
